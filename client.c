196e60193b067d7ba10b3fb1f4a82513940276f0
YH <45375855+Yong-Hoon-In@users.noreply.github.com>
2019-12-10 22:22:54 +0900

    Add files via upload

--- client.c
+++ client.c
@@ -4,38 +4,62 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-void error_handling(char* message);
-int main(int argc, char* argv[]) {
-	int sock;
-	struct sockaddr_in serv_addr;
-	char message[30];
-	int str_len;
+#include <string.h>
+#define BUF_SIZE 200
+void error_handling(char *message);
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[BUF_SIZE];
+    int str_len;
+    char *startmsg="가위바위보 게임을 시작합니다\n";
+    if (argc != 3)
+    {
+        printf("Usage : %s <IP> <Port>\n", argv[0]);
+        exit(1);
+    }
 
-	if (argc != 3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1)
-		error_handling("socket() error");
+    if (sock == -1)
+        error_handling("socket() error");
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-	if (connect(sock, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) == -1)
-		error_handling("connect() error");
-	if (str_len == -1)
-		error_handling("read() error!");
-	printf("Message from server:  %s \n", message);
-	close(sock);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error");
+	
+
+    printf("%s",startmsg);
+    while(1){
+        read(sock, message, BUF_SIZE);
+        printf("%s",message);
+	while(1){
+        scanf("%s",message);
+	if(strcmp(message,"quit")==0)
 	return 0;
+	else if((strcmp(message,"가위")!=0)&&(strcmp(message,"바위")!=0)&&(strcmp(message,"보")!=0)) 
+		printf("다시 입력해주세요");
+	
+	}
+        write(sock, message, BUF_SIZE);
+        read(sock, message, BUF_SIZE);
+        printf("%s",message);
+    }
+
+    close(sock);
+    return 0;
 }
-void error_handling(char *message){
-	fputs(message stderr);
-	fputc('\n', stderr);
-	exit(1);
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
+
