bd90708ae726a24e11b4e1fabc773a5358c7e32a
YH <45375855+Yong-Hoon-In@users.noreply.github.com>
2019-12-11 22:12:04 +0900

    Update client.c

--- client.c
+++ client.c
@@ -12,15 +12,15 @@ int main(int argc, char *argv[])
     int sock;
     struct sockaddr_in serv_addr;
     char message[BUF_SIZE];
-    int str_len;
-    char *startmsg="가위바위보 게임을 시작합니다\n";
+    char message int str_len;
+    char *startmsg = "가위바위보 게임을 시작합니다\n";
     if (argc != 3)
     {
         printf("Usage : %s <IP> <Port>\n", argv[0]);
         exit(1);
     }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
 
     if (sock == -1)
         error_handling("socket() error");
@@ -30,26 +30,26 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
+    while (1)
+    {
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+            error_handling("connect() error");
+        else
+            puts("Connected");
+        printf("%s", startmsg);
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
-        error_handling("connect() error");
-	
-
-    printf("%s",startmsg);
-    while(1){
         read(sock, message, BUF_SIZE);
-        printf("%s",message);
-	while(1){
-        scanf("%s",message);
-	if(strcmp(message,"quit")==0)
-	return 0;
-	else if((strcmp(message,"가위")!=0)&&(strcmp(message,"바위")!=0)&&(strcmp(message,"보")!=0)) 
-		printf("다시 입력해주세요");
-	
-	}
+        printf("%s", message);
+
+        scanf("%s", message);
+        if (strcmp(message, "quit") == 0)
+            return 0;
+        else if ((strcmp(message, "가위") != 0) && (strcmp(message, "바위") != 0) && (strcmp(message, "보") != 0))
+            printf("다시 입력해주세요");
+
         write(sock, message, BUF_SIZE);
         read(sock, message, BUF_SIZE);
-        printf("%s",message);
+        printf("%s", message);
     }
 
     close(sock);
@@ -62,4 +62,3 @@ void error_handling(char *message)
     fputc('\n', stderr);
     exit(1);
 }
-
