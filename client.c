f3b49a08458592da07536cc1f2bbf29e29c8e5ef
YH <45375855+Yong-Hoon-In@users.noreply.github.com>
2019-12-17 11:11:32 +0900

    Update client.c

--- client.c
+++ client.c
@@ -14,6 +14,11 @@ int main(int argc, char *argv[])
     char message[BUF_SIZE];
     char value[BUF_SIZE];
     int str_len;
+    int read_cnt;
+    FILE *fp;
+    char buf[BUF_SIZE];
+    int draw=0,lose=0,win=0;
+
     if (argc != 3)
     {
         printf("Usage : %s <IP> <Port>\n", argv[0]);
@@ -36,30 +41,59 @@ int main(int argc, char *argv[])
     else
         puts("Connected");
 
-    read(sock,message,BUF_SIZE);
+    read(sock, message, BUF_SIZE);
     printf("가위바위보 게임을 시작합니다");
-    printf("%s",message);
+    printf("%s", message);
+    fp = fopen("option.txt", "rwb");
+    while ((read_cnt = read(sd, buf, BUF_SIZE)) != 0)
+        fwrite((void *)buf, 1, read_cnt, fp);
+//   fp=fopen("option.txt","rb");
+    fread(buf,sizeof(buf),1,fp);
+    for(i=0;buf[i]!=NULL;i++){
+        if(buf[i]==0)
+            draw++;
+        else if(buf[i]==1)
+            lose++;
+        else if(buf[i]==2)
+            win++;
+        
+    }
+    fclose(fp);
+    printf("이긴 횟수: %d 진 횟수: %d 비긴 횟수: %d",win,lose,draw);
+    
     while (1)
     {
-        scanf("%s",value);
-        write(sock,value,sizeof(value));
+        scanf("%s", value);
+        write(sock, value, sizeof(value));
         if (strncmp(value, "quit", 3) == 0)
-            {
-                puts("연결을 종료합니다.");
-                close(sock);
-                exit(0);
-            }
-        if (strncmp(value, "0", 1)==0){
+        {
+            puts("연결을 종료합니다.");
+            close(sock);
+            exit(0);
+        }
+        else if (strncmp(value, "0", 1) == 0)
+        {
             printf("낸것 :가위");
+            read(sock, message, BUF_SIZE);
+            printf("%s", message);
         }
-        if (strncmp(value, "1", 1)==0){
+        else if (strncmp(value, "1", 1) == 0)
+        {
             printf("낸것 :바위");
+            read(sock, message, BUF_SIZE);
+            printf("%s", message);
         }
-        if (strncmp(value, "2", 1)==0){
+        else if (strncmp(value, "2", 1) == 0)
+        {
             printf("낸것 :보");
+            read(sock, message, BUF_SIZE);
+            printf("%s", message);
         }
-        read(sock,message,BUF_SIZE);
-        printf("%s",message);
+        else
+        {
+            printf("잘못입력하셧습니다");
+            read(sock, message, BUF_SIZE);
+        }
     }
 
     close(sock);
