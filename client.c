408455c7592f90c35baf62b93d5d05b55411a63d
YH <45375855+Yong-Hoon-In@users.noreply.github.com>
2019-12-17 14:54:29 +0900

    Update client.c

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <string.h>
-#define BUF_SIZE 200
+#define BUF_SIZE 1024
 void error_handling(char *message);
 int main(int argc, char *argv[])
 {
@@ -15,10 +15,8 @@ int main(int argc, char *argv[])
     char value[BUF_SIZE];
     int str_len;
     int read_cnt;
-    FILE *fp;
-    char buf[BUF_SIZE];
     int draw=0,lose=0,win=0;
-
+    int i;
     if (argc != 3)
     {
         printf("Usage : %s <IP> <Port>\n", argv[0]);
@@ -42,17 +40,26 @@ int main(int argc, char *argv[])
         puts("Connected");
 
     read(sock, message, BUF_SIZE);
-    printf("가위바위보 게임을 시작합니다");
-    printf("%s", message);
+    printf("가위바위보 게임을 시작합니다\n");
+    printf("%s\n", message);
 
-    fp = fopen("option.txt", "rwb");
-    while ((read_cnt = read(sock, buf, BUF_SIZE)) != 0)
-        fwrite((void *)buf, 1, read_cnt, fp);
+    
+    read(sock, message, BUF_SIZE);
+	for(i=0;i<BUF_SIZE;i++){
+	    if(message[i]=='0')
+		draw++;
+	    else if(message[i]=='1')
+		lose++;
+	    else if(message[i]=='2')
+		win++;
+	    else
+		continue;
+	}
+    
+printf("당신의 전적은 ::: \n이긴 횟수: %d 진 횟수: %d 비긴 횟수: %d\n",win,lose,draw);
 
-    fgets(buf,sizeof(buf),1,fp);
 
-    fclose(fp);
-    printf("이긴 횟수: %d 진 횟수: %d 비긴 횟수: %d",win,lose,draw);
+    
     
     while (1)
     {
@@ -66,19 +73,19 @@ int main(int argc, char *argv[])
         }
         else if (strncmp(value, "0", 1) == 0)
         {
-            printf("낸것 :가위");
+            printf("낸것 :가위\n");
             read(sock, message, BUF_SIZE);
             printf("%s", message);
         }
         else if (strncmp(value, "1", 1) == 0)
         {
-            printf("낸것 :바위");
+            printf("낸것 :바위\n");
             read(sock, message, BUF_SIZE);
             printf("%s", message);
         }
         else if (strncmp(value, "2", 1) == 0)
         {
-            printf("낸것 :보");
+            printf("낸것 :보\n");
             read(sock, message, BUF_SIZE);
             printf("%s", message);
         }
